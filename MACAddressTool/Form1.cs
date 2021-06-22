3c7de5ed5fd3c96b58124c90fb4b4d8ebb1371f5
Neboer <1653626705@qq.com>
2021-06-22 23:04:51 +0800

    Update open logic, use threading to avoid stuck on start.

--- MACAddressTool/Form1.cs
+++ MACAddressTool/Form1.cs
@@ -13,7 +13,7 @@ using System.Management;
 using Microsoft.Win32;
 using System.Net.NetworkInformation;
 using System.Text.RegularExpressions;
-
+using System.Threading;
 
 namespace MACAddressTool
 {
@@ -110,7 +110,8 @@ namespace MACAddressTool
             /// <summary>
             /// Get the NetworkAddress registry value of this adapter.
             /// </summary>
-            public string RegistryMac {
+            public string RegistryMac
+            {
                 get
                 {
                     try
@@ -178,7 +179,7 @@ namespace MACAddressTool
                             regkey.SetValue("NetworkAddress", value, RegistryValueKind.String);
                         else
                             regkey.DeleteValue("NetworkAddress");
-                        
+
 
                         return true;
                     }
@@ -280,8 +281,9 @@ namespace MACAddressTool
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private List<Adapter> get_adapters_list()
         {
+            List<Adapter> adapters = new List<Adapter> { };
             /* Windows generally seems to add a number of non-physical devices, of which
              * we would not want to change the address. Most of them have an impossible
              * MAC address. */
@@ -289,10 +291,31 @@ namespace MACAddressTool
                     a => Adapter.IsValidMac(a.GetPhysicalAddress().GetAddressBytes(), true)
                 ).OrderByDescending(a => a.Speed))
             {
-                AdaptersComboBox.Items.Add(new Adapter(adapter));
+                adapters.Add(new Adapter(adapter));
             }
-            
+            return adapters;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AdaptersComboBox.Items.Add("Loading...");
             AdaptersComboBox.SelectedIndex = 0;
+            AdaptersComboBox.Enabled = false;
+            Thread t = new Thread(() =>
+            {
+                var adapters = get_adapters_list();
+                AdaptersComboBox.Invoke((MethodInvoker)delegate
+                {
+                    AdaptersComboBox.Items.Clear();
+                    foreach (Adapter adapter in adapters)
+                    {
+                        AdaptersComboBox.Items.Add(adapter);
+                        AdaptersComboBox.Enabled = true;
+                        AdaptersComboBox.SelectedIndex = 0;
+                    }
+                });
+            });
+            t.Start();
         }
 
         /// <summary>
@@ -301,8 +324,12 @@ namespace MACAddressTool
         private void UpdateAddresses()
         {
             Adapter a = AdaptersComboBox.SelectedItem as Adapter;
-            this.CurrentMacTextBox.Text = a.RegistryMac;
-            this.ActualMacLabel.Text = a.Mac;
+            if (a != null)
+            {
+                this.CurrentMacTextBox.Text = a.RegistryMac;
+                this.ActualMacLabel.Text = a.Mac;
+            }
+
         }
 
         private void AdaptersComboBox_SelectedIndexChanged(object sender, EventArgs e)
